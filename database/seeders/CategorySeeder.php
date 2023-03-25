51a72cba6a758e59bbfd375717333e107c8ea72b
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2023-03-25 12:52:53 -0300

    Add CategorySeeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Random',
+        ]);
+        Category::create([
+            'name' => 'Medic',
+        ]);
+        Category::create([
+            'name' => 'Study',
+        ]);
+    }
+}
