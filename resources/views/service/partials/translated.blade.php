a5fc2fd465f48b7dcc91acc5baf4e8949599dcb2
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2023-03-28 09:35:44 -0300

    Fix layout

--- resources/views/service/partials/translated.blade.php
+++ resources/views/service/partials/translated.blade.php
@@ -1,21 +1,17 @@
 <div class="p-8 w-full text-center rounded bg-gray-300">
-    <header class="lg:mb-6 not-format">
+    <header class="not-format">
         <h1 class="text-2xl font-black">Traduzido!</h1>
     </header>
 </div>
-<div class="flex gap-4">
-    <div class="w-1/2">
+<div class="flex flex-row px-0 justify-center space-x-4">
     <a href="{{ url("storage/{$service->target_file}") }}">
         <x-primary-button>
             {{ __('Ver Arquivo') }}
         </x-primary-button>
     </a>
-    </div>
-    <div class="w-1/2">
     <a href="{{ url("storage/{$service->target_file}") }}" download="{{ str_replace('uploads/', '', $service->target_file) }}">
         <x-primary-button>
             {{ __('Download Arquivo') }}
         </x-primary-button>
     </a>
-    </div>
 </div>
